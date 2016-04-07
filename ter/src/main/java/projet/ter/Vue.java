06aa9a8f722ea41d58afa62ec783a114f3717335
MIGAOU <idriss.migaou@gmail.com>
2016-04-07 19:54:01 +0200

    1

--- ter/src/main/java/projet/ter/Vue.java
+++ ter/src/main/java/projet/ter/Vue.java
@@ -1,14 +1,12 @@
 package projet.ter;
 
-
 public class Vue {
 
+	public static void main(String[] args) {
 
-  public static void main(String[] args){
-	  
-	   MaFenetre test = new MaFenetre();
+		// MaFenetre test = new MaFenetre();
 
-
-  }       
+		Fenetre fen = new Fenetre();
+	}
 
 }
